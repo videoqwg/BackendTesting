45c92160bcdd5726b9bef5c3d53955e0e2fe08f4
videoqwg <3080926500@qq.com>
2024-12-28 10:14:23 +0800

    修改jwt工具类

--- src/main/java/com/example/myproject/Util/JwtUtil.java
+++ src/main/java/com/example/myproject/Util/JwtUtil.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Component
@@ -20,10 +21,11 @@ public class JwtUtil {
         this.jwtConfig = jwtConfig;
     }
 
-    public  String generateToken(String username, String avatar) {
+    public  String generateToken(String username, String avatar, List<String> roles) {
         Map<String, Object> claims = new HashMap<>();
         claims.put("username", username);
         claims.put("avatar", avatar);
+        claims.put("roles", roles);
 
         return Jwts.builder()
                 .setClaims(claims) // 设置负载
