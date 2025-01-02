ac05f195ea81893eec19abba5e6a36eb58653694
videoqwg <3080926500@qq.com>
2025-01-02 14:58:36 +0800

    将用户id修改为String类型方便后续开发的数据转换

--- src/main/java/com/example/myproject/Model/User.java
+++ src/main/java/com/example/myproject/Model/User.java
@@ -4,87 +4,16 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
-@Data
+@lombok.Data
 public class User {
-    private Integer userid;
+    private String userid;
     private String username;
     private String password;
     private String email;
     private String phone;
+    private String introduction;
     private LocalDateTime createTime;
     private LocalDateTime updateTime;
     private String avatar;
     private String role;
-
-    public Integer getUserid() {
-        return userid;
-    }
-
-    public void setUserid(Integer userid) {
-        this.userid = userid;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public LocalDateTime getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(LocalDateTime createTime) {
-        this.createTime = createTime;
-    }
-
-    public LocalDateTime getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(LocalDateTime updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
