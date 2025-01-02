7403a1a6586d7b163f423e571cb0b22b4ced33fe
videoqwg <3080926500@qq.com>
2025-01-02 15:01:06 +0800

    添加获取、更改用户头像和更改用户信息的接口

--- src/main/java/com/example/myproject/Service/UserService.java
+++ src/main/java/com/example/myproject/Service/UserService.java
@@ -2,6 +2,11 @@ package com.example.myproject.Service;
 
 import com.example.myproject.Model.Result;
 import com.example.myproject.Model.User;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
 
 public interface UserService {
     User findUser(String username);
@@ -10,4 +15,9 @@ public interface UserService {
     Result info(String token);
     Result logout();
     Result getRoles(User user);
+    Result updateAvatar(String username, MultipartFile avatar);
+    // Result getAvatar(User user);
+    ResponseEntity<Resource> getAvatar(String filename);
+    Result updateUserData(Map<String, String> userForm,User user);
+    Result updateAccountData(Map<String, String> userForm,User user);
 }
